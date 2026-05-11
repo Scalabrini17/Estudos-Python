4c5073f52eaabf8f12c8bef1537e2570536ebf8d
Scalabrini17 <itallomartins2007@gmail.com>
2026-05-11 17:56:35 -0300

    Update Exercises Level 2

---
+++ Level-2_Object-Oriented/Exercises/Ex01.py
@@ -0,0 +1,11 @@
+class Musica:
+    nome = ''
+    artista = ''
+    duracao = ''
+
+musica_dreamer = Musica()
+musica_dreamer.nome = 'Dreamer'
+musica_dreamer.artista = 'Laufey'
+musica_dreamer.duracao = '3:31'
+
+print(vars(musica_dreamer))
