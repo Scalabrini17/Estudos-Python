f1864cca8ae4702ccc955ca682be4fb1e92a1069
Scalabrini17 <itallomartins2007@gmail.com>
2026-05-25 16:58:00 -0300

    Update: finish Exercise07 level-2

--- level-2_Object-Oriented/Exercises/Exercise07/models.py
+++ level-2_Object-Oriented/Exercises/Exercise07/models.py
@@ -9,12 +9,11 @@ class Livro:
     
     # Atividade 2
     def __str__(self):
-        return f'Livro: {self.titulo} | Autor: {self.autor} | Ano de Publicação:  {self.ano_publicacao} | Disponibilidade: {self.disponivel}'
+        return f'Livro: {self.titulo} | Autor: {self.autor} | Ano de Publicação:  {self.ano_publicacao} '
    
     # Atividade 3
-    def emprestar_livro(self):
-        if self.disponivel:
-            self.disponivel = False
+    def emprestar(self):
+        self.disponivel = False
 
 # Atividade 4
     @staticmethod
@@ -25,10 +24,13 @@ class Livro:
 #atividade2
 livro1 = Livro('As Crônicas de Narnia', 'C.S. Lewis', 1950)    
 livro2 = Livro('O Cristianismo Puro e Simples', 'C.S. Lewis', 1952)
+print(f'{livro1} \n {livro2}')
 
 # Atividade 3
 livro3 = Livro('Um Tratado Sobre a Meditação: Um Cristão no Monte', 'Thomas Watson', 1659)
-livro3.emprestar_livro()
+print(f"Antes de emprestar: Livro disponível? {livro3.disponivel}")
+livro3.emprestar()
+print(f"Depois de emprestar: Livro disponível? {livro3.disponivel}")
 
-
-print(f'{livro1} \n {livro2} \n {livro3}')
+#Atividade 4
+Livro.livros = [livro1, livro2, livro3]
